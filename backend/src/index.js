24849a870bf8cc2b9d3c520f38727b5ed78fc8c0
RyanV-Souza <ryanvsouza99@gmail.com>
2021-06-25 15:24:58 -0300

    Aula 05 - Métodos HTTP e Insomnia

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,41 @@
-const express = require('express');
+const express = require('express')
 
-const app = express();
+const app = express()
 
 app.get('/projects', (request, response) => {
-	return response.json({
-		message: "Hello World"
-	});
-});
+	return response.json([
+		'Projeto 1',
+		'Projeto 2',
+		'Projeto 3'
+	])
+})
+
+app.post('/projects', (request, response) => {
+	return response.json([
+		'Projeto 1',
+		'Projeto 2',
+		'Projeto 3',
+		'Projeto 4'
+	])
+})
+
+app.put('/projects', (request, response) => {
+	return response.json([
+		'Projeto 1',
+		'Projeto 2',
+		'Projeto 3',
+		'Projeto 5'
+		
+	])
+})
+
+app.delete('/projects', (request, response) => {
+	return response.json([
+		'Projeto 1',
+		'Projeto 2'
+	])
+})
 
 app.listen(3333, () => {
 	console.log("Backend Started! 👍");
-});
+})
