bdca187c814ec56dd6168dfee808b1d4b6d46231
RyanV-Souza <ryanvsouza99@gmail.com>
2021-06-25 21:11:02 -0300

    Aula 09 - O que é Middleware?

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,18 @@ app.use(express.json())
 
 const projects = []
 
+function logRoutes(request, response, next) {
+	const { method, url } = request
+
+	const route = `[${method.toUpperCase()}] ${url} `
+	
+	console.log(route)
+
+	return next()
+} 
+
+app.use(logRoutes)
+
 app.get('/projects', (request, response) => {
 	const {title} =request.query
 
