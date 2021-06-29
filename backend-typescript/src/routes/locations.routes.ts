2a194bbfe304431b308f15f465cd0bb05f1315a2
RyanV-Souza <ryanvsouza99@gmail.com>
2021-06-29 20:08:59 -0300

    Aula 22 Upload de Imagem(Multer)

--- backend-typescript/src/routes/locations.routes.ts
+++ backend-typescript/src/routes/locations.routes.ts
@@ -1,7 +1,11 @@
 import {Router} from 'express'
+import multer from 'multer'
 import knex  from '../database/connection'
+import multerConfig from '../config/multer'
 
 const locationsRouter = Router()
+
+const upload = multer(multerConfig)
 
 locationsRouter.get('/', async (request, response) =>{
 	const { city, uf, items } = request.query
@@ -86,6 +90,28 @@ locationsRouter.post('/', async (request, response) => {
 		id: location_id,
 		...location
 	})
+})
+
+locationsRouter.put('/:id', upload.single('image'), async (request, response) => {
+	const { id } = request.params
+
+	const image = request.file?.filename
+
+	const location = await knex('locations').where('id', id).first()
+
+	if(!location){
+		return response.status(400).json({message: 'Location not found'})
+	}
+
+	const locationUpdated = {
+		...location,
+		image
+	}
+
+	await knex('locations').update(locationUpdated).where('id', id)
+
+	return response.json(locationUpdated)
+
 })
 
 
